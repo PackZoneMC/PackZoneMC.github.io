fccd321ad26ffc0f0ef6abd6ec9ac344530f0686
PackZoneMC <amyrmhmdpnahy050@gmail.com>
2026-07-16 14:25:01 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+function saveLanguage(language) {
 
+localStorage.setItem("PZMC_language", language);
+
+}
+
+
+
+window.onload = function() {
+
+let language = localStorage.getItem("PZMC_language");
+
+
+if(language) {
+
+let currentPage = window.location.pathname;
+
+
+if(currentPage.endsWith("index.html") || currentPage === "/") {
+
+
+if(language === "fa") {
+
+window.location.href = "fa/index.html";
+
+}
+
+
+if(language === "en") {
+
+window.location.href = "en/index.html";
+
+}
+
+}
+
+}
+
+}
